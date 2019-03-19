99415c91c58c5e1e3b16ca48e5a83b3d0280a54f
rob <robertchen234@gmail.com>
2019-03-18 23:35:18 -0400

    flexbox navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,18 +5,20 @@ import SearchForm from "./SearchForm";
 class NavBar extends Component {
   render() {
     return (
-      <div className="ui menu navBar">
-        <Fragment>
-          <Link to={"/home"} className="item">
-            <span onClick={this.props.clearSearch}>
-              PlayTime!{" "}
-              <span role="img" aria-label="alarm clock">
-                ⏰
+      <div className="ui menu navBar navbar-layout">
+        <div className="navbar-left">
+          <div className="navbar-logo">
+            <Link to={"/home"} className="item">
+              <span onClick={this.props.clearSearch}>
+                PlayTime!{" "}
+                <span role="img" aria-label="alarm clock">
+                  ⏰
+                </span>
               </span>
-            </span>
-          </Link>
+            </Link>
+          </div>
           {this.props.isUserLoggedIn ? (
-            <div className="stats">
+            <div className="navbar-stats">
               <div className="lvl">Lvl {this.props.currentUser.lvl}</div>
               <div className="exp">
                 Exp: {this.props.currentUser.exp} /{" "}
@@ -26,44 +28,54 @@ class NavBar extends Component {
                 Energy: {this.props.currentUser.energy} /{" "}
                 {this.props.currentUser.max_energy}
               </div>
-
-
             </div>
           ) : null}
+        </div>
 
+        <div className="navbar-search">
           <SearchForm
             search={this.props.search}
             changeHandler={this.props.changeHandler}
           />
-        </Fragment>
-        {this.props.isUserLoggedIn ? (
-          <span className="ui menu navright">
-            <Link to={"/profile"} className="item">
-              <span onClick={this.props.clearSearch}>
-                <img
-                  src={this.props.currentUser.profile_img}
-                  alt="profile pic"
-                  className="profile-picture-nav"
-                />
+        </div>
+        <div className="navbar-right">
+          {this.props.isUserLoggedIn ? (
+            <span className="ui menu">
+              <Link to={"/profile"} className="item">
+                <span onClick={this.props.clearSearch}>
+                  <img
+                    src={this.props.currentUser.profile_img}
+                    alt="profile pic"
+                    className="profile-picture-nav"
+                  />
+                </span>
+              </Link>
+              <Link to={"/editProfile"} className="item">
+                <span onClick={this.props.clearSearch}>
+                  Edit Profile
+                </span>
+              </Link>
+              <span className="item pointer" onClick={this.props.logout}>
+                <span onClick={this.props.clearSearch}>
+                  Logout
+                </span>
               </span>
-            </Link>
-            <Link to={"/editProfile"} className="item">
-              <span onClick={this.props.clearSearch}>Edit Profile</span>
-            </Link>
-            <span className="item pointer" onClick={this.props.logout}>
-              <span onClick={this.props.clearSearch}>Logout</span>
             </span>
-          </span>
-        ) : (
-          <span className="ui menu navright">
-            <Link to={"/login"} className="item">
-              <span onClick={this.props.clearSearch}>Login</span>
-            </Link>
-            <Link to={"/signup"} className="item">
-              <span onClick={this.props.clearSearch}>Sign Up</span>
-            </Link>
-          </span>
-        )}
+          ) : (
+            <span className="ui menu">
+              <Link to={"/login"} className="item">
+                <span onClick={this.props.clearSearch}>
+                  Login
+                </span>
+              </Link>
+              <Link to={"/signup"} className="item">
+                <span onClick={this.props.clearSearch}>
+                  Sign Up
+                </span>
+              </Link>
+            </span>
+          )}
+        </div>
       </div>
     );
   }
